fa7523e84b7e53e7f659682c787f49613625911d
Kamilath Rifka S <kamilathrifka@gmail.com>
2026-09-13 02:32:15 +0530

    Fix Chrome startup in CI

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -2,14 +2,22 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private static WebDriver driver;
 
     public static void initDriver() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
 
     public static WebDriver getDriver() {
